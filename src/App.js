2d78f6760d7ba798aa09ff6f953813e696114bc7
maihoangminhbk <61836935+maihoangminhbk@users.noreply.github.com>
2022-03-02 01:00:21 +0700

    Add card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="trello-maihoangminh-master">
+      <nav className='navbar app'>App bar</nav>
+      <nav className='narbar board'> Board Bar</nav>
+      <div className='board-columns'>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://cdn.24h.com.vn/upload/1-2022/images/2022-03-01/VPBank-thong-bao-lui-giai-chay-VPBank-Hanoi-Marathon-giai-chay-1646112509-607-width660height598.jpg' alt='image1'/>
+              Title: Mai Hoang Minh
+            </li>
+            <li>Add in here</li>
+            <li>Add in here</li>
+            <li>Add in here</li>
+            <li>Add in here</li>
+            <li>Add in here</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              {/* <img src='https://cdn.24h.com.vn/upload/1-2022/images/2022-03-01/VPBank-thong-bao-lui-giai-chay-VPBank-Hanoi-Marathon-giai-chay-1646112509-607-width660height598.jpg' alt='image1'/> */}
+            </li>
+            <li>Add in here</li>
+            <li>Add in here</li>
+            <li>Add in here</li>
+            <li>Add in here</li>
+            <li>Add in here</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
