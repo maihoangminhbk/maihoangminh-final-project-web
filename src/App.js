0536a798da48076380c29fe1a07b1ec0eeb391dd
maihoangminhbk <61836935+maihoangminhbk@users.noreply.github.com>
2022-03-03 00:12:55 +0700

    clean architecture

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
+import React from 'react';
 import './App.scss';
+
+// custom component 
+import AppBar from 'components/AppBar/AppBar';
+import BoardBar from 'components/BoardBar/BoardBar';
+import BoardContent from 'components/BoardContent/BoardContent';
 
 function App() {
   return (
     <div className="trello-maihoangminh-master">
-      <nav className='navbar app'>App bar</nav>
-      <nav className='narbar board'> Board Bar</nav>
-      <div className='board-columns'>
-        <div className='column'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src='https://cdn.24h.com.vn/upload/1-2022/images/2022-03-01/VPBank-thong-bao-lui-giai-chay-VPBank-Hanoi-Marathon-giai-chay-1646112509-607-width660height598.jpg' alt='image1'/>
-              Title: Mai Hoang Minh
-            </li>
-            <li>Add in here</li>
-            <li>Add in here</li>
-            <li>Add in here</li>
-            <li>Add in here</li>
-            <li>Add in here</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className='column'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              {/* <img src='https://cdn.24h.com.vn/upload/1-2022/images/2022-03-01/VPBank-thong-bao-lui-giai-chay-VPBank-Hanoi-Marathon-giai-chay-1646112509-607-width660height598.jpg' alt='image1'/> */}
-            </li>
-            <li>Add in here</li>
-            <li>Add in here</li>
-            <li>Add in here</li>
-            <li>Add in here</li>
-            <li>Add in here</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+     
+      
     </div>
   );
 }
---
+++ src/components/Task/Task.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Task.scss';
+
+function Task() {
+    return (
+        <li className="task-item">
+            <img src='https://cdn.24h.com.vn/upload/1-2022/images/2022-03-01/VPBank-thong-bao-lui-giai-chay-VPBank-Hanoi-Marathon-giai-chay-1646112509-607-width660height598.jpg' alt='image1' />
+            Title: Mai Hoang Minh
+        </li>
+    )
+}
+
+export default Task;
